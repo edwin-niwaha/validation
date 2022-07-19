c9e3111d36e09b2df060441a4111186f30d36e48
Edwin <ed107107@gmail.com>
2022-07-20 00:15:32 +0300

    modified files

--- formValidaton.js
+++ formValidaton.js
@@ -1,16 +1,20 @@
 let username = document.forms["RegForm"]["Name"];
 let email = document.forms["RegForm"]["Email"];
-let pwd = document.forms["RegForm"]["pwd"];
-let phone = document.forms["RegForm"]["Telephone"];
-nameRegex = /^[A-Za-z]+$/ // should contain only letters from A-Z and a-z then $ shows end of regex
-emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+let pass = document.forms["RegForm"]["pass"];
+let phone = document.forms["RegForm"]["phone"];
+let ddlanguages = document.forms["RegForm"]["ddlanguages"];
 
-//let nameerr = document.forms["RegForm"]["nameerr"];
+nameRegex = /^[A-Za-z]+$/ // should contain only letters from A-Z and a-z then $ shows end of regex
+emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ //regex for email
+numberRegex = /^[0-9]*$/ // regex for telephone
+passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^()])[A-Za-z\d@#$!%*?&^()]{8,}$/
+alphanumeri = /^[A-Za-z0-9]+/ //expression that takes on letters and numbesrs only but not special character 
+
 
 function validationForm() {
-  //var lowerCaseLetters = /[a-z]/g;
+  //nameRegex = /^[A-Za-z]+$/ validate alphabets only
   if (username.value == "" || !nameRegex.test(username.value)) {
-    alert("Please enter your name with alphabets only");
+    //alert("Please enter your name with alphabets only");
     username.style.border = "5px solid red";
     //username.innerHTML = "Please enter your name";
     username.focus();
@@ -21,57 +25,28 @@ function validationForm() {
     username.focus();
   }
 
-  if (email.value == "") {
-    //alert("Please enter your email");
-    email.style.border = "5px solid red";
-    email.focus();
-    return false;
-  }
-  else {
-    email.style.border = "5px solid green";
-    email.focus();
-  }
+  //validate email
+  //    if (email.value.indexOf("@", 0) < 0 || email.value.indexOf(".", 0) < 0) {
   if (email.value == "" || !emailRegex.test(email.value)) {
     //alert("Please enter a valid email address")
     email.style.border = "5px solid red";
     email.focus();
     return false;
   }
-  /*
-    if (email.value.indexOf("@", 0) < 0 || email.value.indexOf(".", 0) < 0) {
-      //alert("Please enter a valid email address")
-      email.style.border = "5px solid red";
-      email.focus();
-      return false;
-    }
-    else {
-      email.style.border = "5px solid green";
-      email.focus();
-    }
-  */
-  if (pwd.value == "") {
-    //alert("Please enter your email");
-    pwd.style.border = "5px solid red";
-    pwd.focus();
+
+  if (pass.value == "" || !passRegex.test(pass.value)) {
+    // alert("Please enter your password");
+    pass.style.border = "5px solid red";
+    pass.focus();
     return false;
   }
   else {
-    pwd.style.border = "5px solid green";
-    pwd.focus();
+    pass.style.border = "5px solid green";
   }
-      if (email.value.indexOf("@", 0) < 0 || email.value.indexOf(".", 0) < 0) {
-        //alert("Please enter a valid email address")
-        email.style.border = "5px solid red";
-        email.focus();
-        return false;
-      }
-      else {
-        email.style.border = "5px solid green";
-        email.focus();
-      }
 
-  if (phone.value == "" || phone.value.length != 10) {
-    // alert("Please enter a valid phone number");
+
+  if (phone.value == "" || phone.value.length != 10 || !numberRegex.test(phone.value)) {
+    //alert("Please enter a valid phone number");
     phone.style.border = "5px solid red";
     phone.focus();
     return false;
@@ -81,29 +56,13 @@ function validationForm() {
     email.focus();
   }
 
-<<<<<<< HEAD
+  if (ddlanguages.selectedIndex < 1) {
+    //alert("Please select your course.");
+    ddlanguages.style.border = "5px solid red";
+    select.focus();
+    return false;
+  }
+  else {
+    ddlanguages.style.border = "5px solid green";
+  }
 }
-=======
-      if (phone.value == "" || phone.value.length != 10) {
-        // alert("Please enter a valid phone number");
-        phone.style.border = "5px solid red";
-        phone.focus();
-        return false;
-      }
-      else {
-        phone.style.border = "5px solid green";
-        email.focus();
-      }
-
-      if (ddlanguages == "") {
-        // alert("Please select an option");
-        ddlanguages.style.border = "5px solid red";
-        ddlanguages.focus();
-        return false;
-      }
-      else {
-        ddlanguages.style.border = "5px solid green";
-        ddlanguages.focus();
-      }
-    }
->>>>>>> d8b7135e42e4e9d870ba25dc078c62f6ac72474a
